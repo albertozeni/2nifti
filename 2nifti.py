5921cecdacde0a6cba5cb405135545028122ffe1
Alberto Zeni <alberto.zeni@polimi.it>
2023-07-12 16:43:39 +0200

    Removed useless raw

--- 2nifti.py
+++ 2nifti.py
@@ -10,6 +10,5 @@ import cv2
 imgnp = np.asarray(Image.open(sys.argv[1]))
 # img = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR)
 # imgnp = np.vstack(img)
-imgplot = plt.imshow(imgnp)
 new_image = nib.Nifti1Image(imgnp, affine=np.eye(4))
 nib.save(new_image, sys.argv[2])
